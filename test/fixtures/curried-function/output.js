34c3223dfaed293c9445256d0353e27cab9d9c12
Petter Sæther Moen <pettersmoen@gmail.com>
2020-09-28 17:55:03 +0200

    :white_check_mark: Fikse feilaktig test

--- test/fixtures/curried-function/output.js
+++ test/fixtures/curried-function/output.js
@@ -1,14 +1,18 @@
 function curry(fn) {
   const numParamsRequired = fn.length;
+
   function curryFactory(params) {
     return function (...args) {
       const newParams = params.concat(args);
+
       if (newParams.length >= numParamsRequired) {
         return fn(...newParams);
       }
+
       return curryFactory(newParams);
     };
   }
+
   return curryFactory([]);
 }
 
